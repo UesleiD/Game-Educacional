65cab5ae8e1c361efd383e1a8917f706f4ce01d5
Ueslei Dalmina <ueshow.ut@gmail.com>
2021-06-23 00:07:12 -0300

    Adicionando arquivo Log

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
-import pygame
-import random
+import pygame,time,random
+
 pygame.init()
+arquivo = open ("dados.txt","a")
+nome = input(str( "Qual é o seu Nome? " ))
+gmail = input(str( "Qual é o seu E-mail? " )) 
+arquivo.write(nome + " "+ gmail + " \n") 
+arquivo.close()
 
+
+certa = pygame.mixer.music.load("assets/certa.wav")
 icone = pygame.image.load("assets/icone.png")
-
 pygame.display.set_icon(icone)
 pygame.display.set_caption("It's Raining Garbage")
 largura = 800
@@ -26,7 +32,30 @@ preto = (0, 0, 0,)
 branco = (255, 255, 255)
 #CORES {FIM}
 
+def mostrarsacospegos(sacospegados):
+    fonte = pygame.font.SysFont(None,25)
+    texto = fonte.render("Lixos Coletados:"+str(sacospegados), True, preto)
+    display.blit(texto, (0, 0))
+
+def mostrarlixosperdidos(lixosperdidos):
+    fonte = pygame.font.SysFont(None,25)
+    texto = fonte.render("Lixos perdidos:"+str(lixosperdidos), True, preto)
+    display.blit(texto, (650, 0))
+
+def mostraperdeu():
+    fonte = pygame.font.SysFont(None,50)
+    texto = fonte.render("Você deixou 3 sacos de lixo cair!", True, preto)
+    display.blit(texto, (130, 300))   
+    pygame.display.update()
+
+
+
+
+
+
 def jogo():
+    pygame.mixer.music.load("assets/fundomusica.mp3")
+    pygame.mixer.music.play(-1) #música fica no loop
     lixeiraX = largura* 0.40
     lixeiraY = altura* 0.75
     movimentoX = 0
@@ -37,7 +66,7 @@ def jogo():
     velocidadesacolixo = 4 
 
     sacospegados = 0
-
+    lixosperdidos = 0
     while True:
         #bloco de código que verifica a interação do usuário {INI}
         for evento in pygame.event.get():
@@ -70,21 +99,32 @@ def jogo():
         #quando ele atravessa a tela ele cai de outra posição {INI}
         if sacolixoY > altura:
             sacolixoY = -80
-            velocidadesacolixo += 1
+            velocidadesacolixo += float(0.3)
             sacolixoX = random.randrange(0, largura -150)
+            lixosperdidos +=1
         #quando ele atravessa a tela ele cai de outra posição {FIM}
         
         #Analise de colisão para contagem
         if lixeiraY < sacolixoY + sacolixoaltura:
-            if lixeiraX < sacolixoX and lixeiraX+180 > sacolixoX or sacolixoX+110 > lixeiraX and sacolixoX+110 < lixeiraX+180:
+            
+            if lixeiraX < sacolixoX and lixeiraX+100 > sacolixoX or sacolixoX+60 > lixeiraX and sacolixoX+60 < lixeiraX+100:
+                sacolixoY = -80
+                sacolixoX = random.randrange(0, largura -150)
                 
+                sacospegados += 1
                 
-                sacospegados = sacospegados + 1
+        if lixosperdidos >= 3:
+            mostraperdeu() 
+            time.sleep(3)
+            pygame.quit()
+             
+
+       
         
-                
-            print(sacospegados)
         
         
+        mostrarsacospegos(sacospegados)
+        mostrarlixosperdidos(lixosperdidos)
         pygame.display.update()
         fps.tick(60)
 
